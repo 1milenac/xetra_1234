ebfddc1428cd76a85ad18022116a14d09d079496
geoinf1 <milenacudak96@gmail.com>
2022-10-30 17:12:16 +0100

    added read_csv_to_df and unittests

--- xetra/common/s3.py
+++ xetra/common/s3.py
@@ -2,11 +2,15 @@
 import os
 import logging
 import boto3
+import pandas as pd
+from io import StringIO
+
 
 class S3BucketConnector():
     """
     Class for interacting with S3 buckets
     """
+
     def __init__(self, access_key: str, secret_key: str, endpoint_url: str, bucket: str):
         """
         Constructor for S3BucketConnector
@@ -19,7 +23,7 @@ class S3BucketConnector():
         self.endpoint_url = endpoint_url
         self.session = boto3.Session(aws_access_key_id=os.environ[access_key],
                                      aws_secret_access_key=os.environ[secret_key])
-        self._s3 = self.session.resource(service_name='s3', endpoint_url = endpoint_url)
+        self._s3 = self.session.resource(service_name='s3', endpoint_url=endpoint_url)
         self._bucket = self._s3.Bucket(bucket)
 
     def list_files_in_prefix(self, prefix: str):
@@ -31,12 +35,19 @@ class S3BucketConnector():
         files = [obj.key for obj in self._bucket.objects.filter(Prefix=prefix)]
         return files
 
-    def read_csv_to_df(self):
+    def read_csv_to_df(self, key: str, encoding: str = 'utf-8', sep: str = ','):
         """
-
-        :return:
+        reading a csv from bucket S3 to df
+        :param key:key of the file that should be read
+        :param encoding: encoding of the data inside the csv
+        :param sep: seperator of the csv file
+        :return: df
         """
-        pass
+        self._logger.info('Reading file %s/%s/%s', self.endpoint_url, self._bucket.name, key)
+        csv_obj = self._bucket.Object(key=key).get().get('Body').read().decode(encoding)
+        data = StringIO(csv_obj)
+        data_frame = pd.read_csv(data, sep=sep)
+        return data_frame
 
     def write_df_to_s3(self):
         pass
--- xetra/common/test_s3.py
+++ xetra/common/test_s3.py
@@ -37,7 +37,7 @@ class TestS3BucketConnectorMethods(unittest.TestCase):
                               })
         self.s3_bucket = self.s3.Bucket(self.s3_bucket_name)
         # Creating a testing instance
-        self.s3_bucket_connector = S3BucketConnector(
+        self.s3_bucket_conn = S3BucketConnector(
             self._s3_access_key, self._s3_secret_key, self.s3_endpoint_url, self.s3_bucket_name
         )
 
@@ -63,7 +63,7 @@ class TestS3BucketConnectorMethods(unittest.TestCase):
         self.s3_bucket.put_object(Body=csv_content, Key=key1_exp)
         self.s3_bucket.put_object(Body=csv_content, Key=key2_exp)
         # Method execution
-        list_result = self.s3_bucket_connector.list_files_in_prefix(prefix_exp)
+        list_result = self.s3_bucket_conn.list_files_in_prefix(prefix_exp)
         # Tests after method execution
         self.assertEqual(len(list_result), 2)
         self.assertIn(key1_exp, list_result)
@@ -89,9 +89,44 @@ class TestS3BucketConnectorMethods(unittest.TestCase):
         # Expected results
         prefix_exp = 'no-prefix/'
         # Method execution
-        list_result = self.s3_bucket_connector.list_files_in_prefix(prefix_exp)
+        list_result = self.s3_bucket_conn.list_files_in_prefix(prefix_exp)
         # Tests after method execution
         self.assertTrue(not list_result)
+
+    def test_read_csv_to_df(self):
+        """
+        Tests the read_csv_to_df method
+        """
+        # Expected results
+        key_exp = 'test.csv'
+        col1_exp = 'col1'
+        col2_exp = 'col2'
+        val1_exp = 'val1'
+        val2_exp = 'val2'
+        log_exp = f'Reading file {self.s3_endpoint_url}/{self.s3_bucket_name}/{key_exp}'
+        # Test init
+        csv_content = f'{col1_exp},{col2_exp}\n{val1_exp},{val2_exp}'
+        self.s3_bucket.put_object(Body=csv_content, Key=key_exp)
+        # Method execution
+        with self.assertLogs() as logm:
+            df_result = self.s3_bucket_conn.read_csv_to_df(key_exp)
+            # log test after the method execution
+            self.assertIn(log_exp, logm.output[0])
+        # Test after method execution
+        self.assertEqual(df_result.shape[0], 1)
+        self.assertEqual(df_result.shape[1], 2)
+        self.assertEqual(val1_exp, df_result[col1_exp][0])
+        self.assertEqual(val2_exp, df_result[col2_exp][0])
+        # Cleanup after tests
+        self.s3_bucket.delete_objects(
+            Delete={
+                'Objects': [
+                    {
+                        'Key': key_exp
+                    }
+                ]
+            }
+        )
 
 
 if __name__ == '__main__':
