6ce6769337a949420ae6b9ae88dff8bab109f91b
geoinf1 <milenacudak96@gmail.com>
2022-10-31 11:42:31 +0100

    added return_date_list and unittests

--- xetra/common/meta_process.py
+++ xetra/common/meta_process.py
@@ -2,7 +2,7 @@
 Methods for processing the meta file
 """
 import collections
-from datetime import datetime
+from datetime import datetime, timedelta
 from xetra.common.s3 import S3BucketConnector
 from xetra.common.constants import MetaProcessFormat
 from xetra.common.custom_exceptions import WrongMetaFileException
@@ -42,9 +42,41 @@ class MetaProcess:
         s3_bucket_meta.write_df_to_s3(df_all, meta_key, MetaProcessFormat.META_FILE_FORMAT.value)
         return True
 
-
-
-
     @staticmethod
-    def return_date_list():
-        pass
+    def return_date_list(first_date: str, meta_key : str, s3_bucket_meta: S3BucketConnector):
+        """
+        Creating a list of dates based on the input first_date and already processed dates in the meta file
+        :param first_date: the earliest date that shouldbe processed
+        :param meta_key: key of the meta file on the S3 bucket
+        :param s3_bucket_meta: S3BucketConnector for the bucket with the same meta file
+        :return:
+        min_date: first date that should be processed
+        return_date_list: list of dates from min_date till today
+        """
+        start = datetime.strftime(first_date,
+                                  MetaProcessFormat.META_DATE_FORMAT.value).date() - timedelta(days=1)
+        today = datetime.today().date()
+        try:
+            # if meta file exists, create return_date_list using the content of meta file
+            # Reading meta file
+            df_meta = s3_bucket_meta.read_csv_to_df(meta_key)
+            # creating a list of dates from first_date until today
+            dates = [(start + timedelta(days=x)) for x in range(0, (today - start).days + 1)]
+            src_dates = set(pd.to_datetime(df_meta[MetaProcessFormat.META_SOURCE_DATE_COL.value]).dt.date)
+            dates_missing = set(dates[1:]) - src_dates
+            if dates_missing:
+                # Determining the earliest date that should be processed
+                min_date = min(set(dates[1:]) - src_dates) - timedelta(days=1)
+                return_dates = [date.strftime(MetaProcessFormat.META_DATE_FORMAT.value) for date in dates if date >= min_date]
+                return_min_date = (min_date + timedelta(days=1)).strftime(MetaProcessFormat.META_DATE_FORMAT.value)
+            else:
+                # setting values for the earliestt date and list of dates
+                return_dates = []
+                return_min_date = datetime(2200, 1, 1).date().strftime(MetaProcessFormat.META_DATE_FORMAT.value)
+
+        except s3_bucket_meta.session.client('s3').exceptions.NoSuchKey:
+            # no meta file found -> creating a date list from first_day -1 until today
+            return_dates = [(start + timedelta(days=x)).strftime(MetaProcessFormat.META_DATE_FORMAT.value) for x in range(0, (today - start).days + 1)]
+            return_min_date = first_date
+
+        return return_min_date, return_dates
--- xetra/common/test_meta_process.py
+++ xetra/common/test_meta_process.py
@@ -4,14 +4,17 @@ import unittest
 import boto3
 from moto import mock_s3
 from xetra.common.s3 import S3BucketConnector
+from xetra.common.meta_process import MetaProcess
 from xetra.common.constants import MetaProcessFormat
 from datetime import datetime, timedelta
+
 
 class TestMetaProcessMethods(unittest.TestCase):
     """
     Testing the MetaProcess class
     --in progress--
     """
+
     def setUp(self) -> None:
         """
                 Setting up the environment
@@ -33,13 +36,14 @@ class TestMetaProcessMethods(unittest.TestCase):
                               CreateBucketConfiguration={
                                   'LocationConstraint': 'eu-central-1'
                               })
+        self.s3_bucket_meta = S3BucketConnector
         self.s3_bucket = self.s3.Bucket(self.s3_bucket_name)
         # Creating a testing instance
         self.s3_bucket_conn = S3BucketConnector(
             self._s3_access_key, self._s3_secret_key, self.s3_endpoint_url, self.s3_bucket_name
         )
-        self.dates = [(datetime.today().date() - timedelta(days=day))\
-                      .strftime(MetaProcessFormat.META_DATE_FORMAT.value) for day in range(8)]
+        self.dates = [(datetime.today().date() - timedelta(days=day)) \
+                          .strftime(MetaProcessFormat.META_DATE_FORMAT.value) for day in range(8)]
 
     def tearDown(self):
         """
@@ -50,5 +54,33 @@ class TestMetaProcessMethods(unittest.TestCase):
 
     def test_update_meta_file_no_meta_file(self):
         pass
+
     def test_update_meta_file_empty_date_list(self):
         pass
+
+    def test_return_date_list_no_meta_file(self):
+        """
+        Tests teh return_date_list method with no meta file
+        """
+        # Expected results
+        date_list_exp = [
+            (datetime.today().date() - timedelta(days=day)) \
+                .strftime(MetaProcessFormat.META_DATE_FORMAT.value) for day in range(4)
+        ]
+        min_date_exp = (datetime.today().date() - timedelta(days=2)).strftime(MetaProcessFormat.META_DATE_FORMAT.value)
+        # Test init
+        first_date = min_date_exp
+        meta_key = 'meta.csv'
+        # Method exec
+        min_date_return, date_list_return = MetaProcess.return_date_list(first_date, meta_key, self.s3_bucket_meta)
+        # Test after method exec
+        self.assertEqual(set(date_list_exp), set(date_list_return))
+        self.assertEqual(min_date_exp, min_date_return)
+
+    def test_return_date_list_meta_file_ok(self):
+        pass
+
+    def test_return_date_list_meta_file_wrong(self):
+        pass
+
+    
